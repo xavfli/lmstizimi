ac9155831347151712e599bf8a928c8eb5ec4ed3
Nuriddinov Boburbek(VO1D) <132753283+xavfli@users.noreply.github.com>
2025-03-14 16:00:38 +0500

    Fix static files

--- Conf/settings.py
+++ Conf/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['.vo1d.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com','.vo1d.com', '127.0.0.1']
 
 
 # Application definition
